c3933c655f7b7790ad2ed313d8a2160b9e49ed21
NickNgugi <21624@ICF2023089-VM>
2017-06-03 01:33:44 -0400

    Added basic flow

---
+++ WebUI/App_Start/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System.Configuration;
+using System.Globalization;
+using System.Threading.Tasks;
+
+[assembly:OwinStartup(typeof(WebUI.App_Start.Startup))]
+namespace WebUI.App_Start
+{
+    public class Startup
+    {
+        private static string clientId = ConfigurationManager.AppSettings["iad:ClientId"];
+        private static string aadInstance = ConfigurationManager.AppSettings["iad:AADInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["iad:Tenant"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["iad:PostLogoutRedirectUri"];
+
+        string authority = string.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        AuthenticationFailed = context =>
+                        {
+                            context.HandleResponse();
+                            context.Response.Redirect("Error/message=" + context.Exception.Message);
+                            return Task.FromResult(0);
+                        }
+                    }
+                });
+        }
+    }
+}
---
+++ WebUI/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                        new AuthenticationProperties { RedirectUri = "/" },
+                        OpenIdConnectAuthenticationDefaults.AuthenticationType
+                    );
+            }
+        }
+        public void SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                CookieAuthenticationDefaults.AuthenticationType
+            );
+        }
+    }
+}
